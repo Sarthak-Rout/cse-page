5c3f913d3c15fe909dd44b298cd638f0079b1e2e
mahesdwivedi <mahesdwivedi@gmail.com>
2018-09-19 05:43:18 +0530

    test

--- nboard/app.js
+++ nboard/app.js
@@ -9,6 +9,13 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+var mongoose = require('mongoose');
+
+var mongoDB = 'mongodb://127.0.0.1:27017/notice';
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- nboard/routes/index.js
+++ nboard/routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const Notice = require('../models/notice')
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -20,6 +21,17 @@ router.get('/login', function(req, res) {
 
 router.get('/notice', function(req, res) {
   res.render('notice');
+});
+router.get('/enter', function(req, res) {
+  res.render('enter');
+});
+router.post('/enter', function(req, res) {
+  console.log(req.body);
+  let newNotice = new Notice(req.body);
+  console.log(newNotice);
+  newNotice.save()
+    .then(res.send("saved succesfully"))
+    .catch((err) => console.log(err))
 });
 
 module.exports = router;
